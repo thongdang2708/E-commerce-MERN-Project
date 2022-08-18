868185fe85cc70e94bb8c6be959f1ee5c284e16b
thongdang2708 <duythong2708@gmail.com>
2022-08-18 21:44:27 +0300

    checked

--- frontend/src/components/Private/Private.jsx
+++ frontend/src/components/Private/Private.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import authStatus from '../../hooks/AuthStatus';
 import { Navigate } from 'react-router-dom';
 import Spinner from '../layouts/Spinner';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from "react-redux";
 const PrivateForUser = () => {
 
-    let {loggedIn, checkStatus} = authStatus();
+    let [loggedIn, setLoggedIn] = useState(false);
+    let [checkStatus, setCheckStatus] = useState(true);
+
+    let {user} = useSelector(state => state.user);
+
+    useEffect(() => {
+
+        if (user) {
+            setLoggedIn(true);
+        }  else {
+            setLoggedIn(false);
+        }
+
+        setCheckStatus(false);
+
+
+    },[user]);
 
     if (checkStatus) {
         return (<Spinner />)
